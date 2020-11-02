1946483a5903bc2e22ab2822d0441f6a54f94add
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-02 13:17:24 +0100

    Set MainWindow to startup form.

---
+++ NemzetiVirusbolt/NemzetiVirusbolt/Views/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace NemzetiVirusbolt.Views
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
