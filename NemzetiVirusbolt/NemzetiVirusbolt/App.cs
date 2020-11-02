a9c892dc39e075b9e9e77702a8b4fc513e46eea7
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-02 14:50:48 +0100

    Set project under .NET Core configuration.

---
+++ NemzetiVirusbolt/NemzetiVirusbolt/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NemzetiVirusbolt.Views;
+
+namespace NemzetiVirusbolt
+{
+    public static class App
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services => { services.AddScoped<MainWindow>(); })
+                .Build();
+
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.Run(mainWindow);
+        }
+    }
+}
