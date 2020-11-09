30fcc7b2cd23b284cd65a59c13d8b6d88d07e169
Noémi László <geonoemilaszlo@gmail.com>
2020-11-09 19:54:26 +0100

    new tabs juhuu

--- NemzetiVirusbolt/NemzetiVirusbolt.DesktopClient/Views/Components/TabControlComponent.Designer.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.DesktopClient/Views/Components/TabControlComponent.Designer.cs
@@ -31,6 +31,8 @@
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPageStock = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabControl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,40 +40,57 @@
             // 
             this.tabControl.Controls.Add(this.tabPageStock);
             this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage3);
             this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(819, 729);
+            this.tabControl.Size = new System.Drawing.Size(1092, 897);
             this.tabControl.TabIndex = 0;
             // 
             // tabPageStock
             // 
-            this.tabPageStock.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStock.Location = new System.Drawing.Point(4, 25);
+            this.tabPageStock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageStock.Name = "tabPageStock";
-            this.tabPageStock.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageStock.Size = new System.Drawing.Size(811, 703);
+            this.tabPageStock.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPageStock.Size = new System.Drawing.Size(1084, 868);
             this.tabPageStock.TabIndex = 0;
             this.tabPageStock.Text = "Raktárkészlet";
             this.tabPageStock.UseVisualStyleBackColor = true;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(811, 703);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1084, 868);
             this.tabPage1.TabIndex = 1;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Új termék felvétele";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1084, 868);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "Keresés";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            
             // 
             // TabControlComponent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tabControl);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TabControlComponent";
-            this.Size = new System.Drawing.Size(819, 729);
+            this.Size = new System.Drawing.Size(1092, 897);
             this.Load += new System.EventHandler(this.TabControlComponent_Load);
             this.tabControl.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -83,5 +102,7 @@
         private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage tabPageStock;
         private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
