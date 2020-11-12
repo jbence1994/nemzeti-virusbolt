be86b83c73c24e35f6be53e50a0bf16579297126
Noémi László <geonoemilaszlo@gmail.com>
2020-11-12 14:10:11 +0100

    addproduct in StockComponent.cs

--- NemzetiVirusbolt/NemzetiVirusbolt.DesktopClient/Views/Components/StockComponent.Designer.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.DesktopClient/Views/Components/StockComponent.Designer.cs
@@ -30,7 +30,19 @@
         {
             this.dataGridViewStock = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.descriptionBox = new System.Windows.Forms.TextBox();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.netPriceLabel = new System.Windows.Forms.Label();
+            this.supplierLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.supplierBox = new System.Windows.Forms.TextBox();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridViewStock
@@ -45,11 +57,13 @@
             this.dataGridViewStock.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewStock.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewStock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridViewStock.MultiSelect = false;
             this.dataGridViewStock.Name = "dataGridViewStock";
             this.dataGridViewStock.ReadOnly = true;
+            this.dataGridViewStock.RowHeadersWidth = 51;
             this.dataGridViewStock.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridViewStock.Size = new System.Drawing.Size(811, 447);
+            this.dataGridViewStock.Size = new System.Drawing.Size(1081, 444);
             this.dataGridViewStock.TabIndex = 0;
             // 
             // panel1
@@ -57,21 +71,132 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(3, 453);
+            this.panel1.Controls.Add(this.descriptionBox);
+            this.panel1.Controls.Add(this.descriptionLabel);
+            this.panel1.Controls.Add(this.addButton);
+            this.panel1.Controls.Add(this.netPriceLabel);
+            this.panel1.Controls.Add(this.supplierLabel);
+            this.panel1.Controls.Add(this.quantityLabel);
+            this.panel1.Controls.Add(this.nameLabel);
+            this.panel1.Controls.Add(this.priceBox);
+            this.panel1.Controls.Add(this.supplierBox);
+            this.panel1.Controls.Add(this.quantityBox);
+            this.panel1.Controls.Add(this.nameBox);
+            this.panel1.Location = new System.Drawing.Point(4, 452);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(805, 247);
+            this.panel1.Size = new System.Drawing.Size(1073, 410);
             this.panel1.TabIndex = 1;
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.Location = new System.Drawing.Point(247, 181);
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.Size = new System.Drawing.Size(248, 22);
+            this.descriptionBox.TabIndex = 31;
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.descriptionLabel.Location = new System.Drawing.Point(21, 183);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(62, 20);
+            this.descriptionLabel.TabIndex = 30;
+            this.descriptionLabel.Text = "Leírás";
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addButton.Location = new System.Drawing.Point(314, 329);
+            this.addButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(107, 48);
+            this.addButton.TabIndex = 29;
+            this.addButton.Text = "Hozzáad";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // netPriceLabel
+            // 
+            this.netPriceLabel.AutoSize = true;
+            this.netPriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.netPriceLabel.Location = new System.Drawing.Point(23, 131);
+            this.netPriceLabel.Name = "netPriceLabel";
+            this.netPriceLabel.Size = new System.Drawing.Size(60, 20);
+            this.netPriceLabel.TabIndex = 28;
+            this.netPriceLabel.Text = "Ár (ft)";
+            // 
+            // supplierLabel
+            // 
+            this.supplierLabel.AutoSize = true;
+            this.supplierLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.supplierLabel.Location = new System.Drawing.Point(23, 237);
+            this.supplierLabel.Name = "supplierLabel";
+            this.supplierLabel.Size = new System.Drawing.Size(65, 20);
+            this.supplierLabel.TabIndex = 27;
+            this.supplierLabel.Text = "Gyártó";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.quantityLabel.Location = new System.Drawing.Point(23, 79);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(98, 20);
+            this.quantityLabel.TabIndex = 26;
+            this.quantityLabel.Text = "Mennyiség";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.Location = new System.Drawing.Point(23, 31);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(41, 20);
+            this.nameLabel.TabIndex = 25;
+            this.nameLabel.Text = "Név";
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(247, 129);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(248, 22);
+            this.priceBox.TabIndex = 24;
+            // 
+            // supplierBox
+            // 
+            this.supplierBox.Location = new System.Drawing.Point(247, 235);
+            this.supplierBox.Name = "supplierBox";
+            this.supplierBox.Size = new System.Drawing.Size(248, 22);
+            this.supplierBox.TabIndex = 23;
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Location = new System.Drawing.Point(247, 77);
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(248, 22);
+            this.quantityBox.TabIndex = 22;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(247, 29);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(248, 22);
+            this.nameBox.TabIndex = 21;
             // 
             // StockComponent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridViewStock);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "StockComponent";
-            this.Size = new System.Drawing.Size(811, 703);
+            this.Size = new System.Drawing.Size(1081, 865);
             this.Load += new System.EventHandler(this.StockComponent_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -80,5 +205,16 @@
 
         private System.Windows.Forms.DataGridView dataGridViewStock;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox descriptionBox;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label netPriceLabel;
+        private System.Windows.Forms.Label supplierLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox supplierBox;
+        private System.Windows.Forms.TextBox quantityBox;
+        private System.Windows.Forms.TextBox nameBox;
     }
 }
--- NemzetiVirusbolt/NemzetiVirusbolt.DesktopClient/Views/Components/StockComponent.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.DesktopClient/Views/Components/StockComponent.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
+using NemzetiVirusbolt.Core;
 using NemzetiVirusbolt.Core.Repositories;
+using NemzetiVirusbolt.Core.Models;
 
 namespace NemzetiVirusbolt.DesktopClient.Views.Components
 {
@@ -8,15 +10,42 @@ namespace NemzetiVirusbolt.DesktopClient.Views.Components
     {
         private readonly IStockRepository _stockRepository;
 
-        public StockComponent(IStockRepository stockRepository)
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public StockComponent(IStockRepository stockRepository,
+            IProductRepository productRepo, IUnitOfWork unitOfWork)
         {
             InitializeComponent();
+            this._productRepository = productRepo;
+            this._unitOfWork = unitOfWork;
             _stockRepository = stockRepository;
         }
 
         private void StockComponent_Load(object sender, EventArgs e)
         {
             dataGridViewStock.DataSource = _stockRepository.GetStocks();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            String Name = nameBox.Text;
+            double Price = double.Parse(priceBox.Text);
+            String Unit = quantityBox.Text;
+            String Description = descriptionBox.Text;
+            Supplier Suppliers = new Supplier();
+            Suppliers.Name = supplierBox.Text;
+
+            Product product = new Product();
+            product.Name = Name;
+            product.Price = Price;
+            product.Unit = Unit;
+            product.Description = Description;
+            product.Supplier = Suppliers;
+
+            _productRepository.AddProduct(product);
+            _unitOfWork.Complete();
         }
     }
 }
