27af9fed36ec0aee49626dbee98cb4322c430a64
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-12 21:59:33 +0100

    Update ProductDto.cs

--- NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Dtos/ProductDto.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Dtos/ProductDto.cs
@@ -4,12 +4,12 @@ namespace NemzetiVirusbolt.Desktop.Dtos
 {
     public class ProductDto
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Price { get; set; }
-        public string Unit { get; set; }
-        public string Description { get; set; }
-        public string SupplierName { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Price { get; private set; }
+        public string Unit { get; private set; }
+        public string Description { get; private set; }
+        public string SupplierName { get; private set; }
 
         public static ProductDto ToDto(Product product)
         {
