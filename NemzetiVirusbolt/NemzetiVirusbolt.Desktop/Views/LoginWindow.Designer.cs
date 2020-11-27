79aa35f481da5e9b7949547adc6d38593e76ea1f
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-27 16:31:08 +0100

    Update LoginWindow.Designer.cs

--- NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Views/LoginWindow.Designer.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Views/LoginWindow.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginWindow));
             this.panelMain = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
             this.buttonExit = new System.Windows.Forms.Button();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
@@ -41,6 +42,7 @@
             // 
             // panelMain
             // 
+            this.panelMain.Controls.Add(this.labelTitle);
             this.panelMain.Controls.Add(this.buttonExit);
             this.panelMain.Controls.Add(this.buttonLogin);
             this.panelMain.Controls.Add(this.textBoxPassword);
@@ -50,16 +52,26 @@
             this.panelMain.Location = new System.Drawing.Point(0, 0);
             this.panelMain.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(915, 647);
+            this.panelMain.Size = new System.Drawing.Size(800, 500);
             this.panelMain.TabIndex = 0;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelTitle.Location = new System.Drawing.Point(318, 132);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(240, 50);
+            this.labelTitle.TabIndex = 6;
+            this.labelTitle.Text = "Raktárkészlet nyilvántartó\r\nv0.1 Demo\r\n";
             // 
             // buttonExit
             // 
-            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonExit.Location = new System.Drawing.Point(439, 375);
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonExit.Location = new System.Drawing.Point(318, 337);
             this.buttonExit.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(393, 35);
+            this.buttonExit.Size = new System.Drawing.Size(392, 35);
             this.buttonExit.TabIndex = 4;
             this.buttonExit.Text = "Kilépés";
             this.buttonExit.UseVisualStyleBackColor = true;
@@ -67,11 +79,11 @@
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonLogin.Location = new System.Drawing.Point(439, 333);
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonLogin.Location = new System.Drawing.Point(318, 296);
             this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(393, 35);
+            this.buttonLogin.Size = new System.Drawing.Size(392, 35);
             this.buttonLogin.TabIndex = 3;
             this.buttonLogin.Text = "Belépés";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -79,21 +91,23 @@
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBoxPassword.Location = new System.Drawing.Point(439, 297);
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPassword.Location = new System.Drawing.Point(318, 255);
             this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxPassword.Multiline = true;
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
-            this.textBoxPassword.Size = new System.Drawing.Size(392, 26);
+            this.textBoxPassword.Size = new System.Drawing.Size(392, 35);
             this.textBoxPassword.TabIndex = 2;
             // 
             // textBoxUsername
             // 
-            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBoxUsername.Location = new System.Drawing.Point(439, 260);
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxUsername.Location = new System.Drawing.Point(318, 214);
             this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxUsername.Multiline = true;
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(392, 26);
+            this.textBoxUsername.Size = new System.Drawing.Size(392, 35);
             this.textBoxUsername.TabIndex = 1;
             // 
             // pictureBoxLogo
@@ -103,7 +117,7 @@
             this.pictureBoxLogo.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBoxLogo.Name = "pictureBoxLogo";
-            this.pictureBoxLogo.Size = new System.Drawing.Size(368, 647);
+            this.pictureBoxLogo.Size = new System.Drawing.Size(245, 500);
             this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxLogo.TabIndex = 0;
             this.pictureBoxLogo.TabStop = false;
@@ -113,7 +127,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(915, 647);
+            this.ClientSize = new System.Drawing.Size(800, 500);
             this.Controls.Add(this.panelMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -138,5 +152,6 @@
         private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.TextBox textBoxUsername;
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelTitle;
     }
 }
