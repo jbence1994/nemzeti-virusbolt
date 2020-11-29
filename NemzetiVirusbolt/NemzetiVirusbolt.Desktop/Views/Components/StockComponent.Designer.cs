cd8878054f1816bc5d6344dda4a07669b946bf4b
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-29 21:33:08 +0100

    Update StockComponent.Designer.cs

--- NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Views/Components/StockComponent.Designer.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Views/Components/StockComponent.Designer.cs
@@ -35,14 +35,21 @@ namespace NemzetiVirusbolt.Desktop.Views.Components
             // 
             // dataGridViewStock
             // 
+            this.dataGridViewStock.AllowUserToAddRows = false;
+            this.dataGridViewStock.AllowUserToDeleteRows = false;
+            this.dataGridViewStock.AllowUserToResizeRows = false;
             this.dataGridViewStock.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewStock.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewStock.BackgroundColor = System.Drawing.SystemColors.AppWorkspace;
             this.dataGridViewStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewStock.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewStock.MultiSelect = false;
             this.dataGridViewStock.Name = "dataGridViewStock";
+            this.dataGridViewStock.ReadOnly = true;
             this.dataGridViewStock.RowTemplate.Height = 25;
+            this.dataGridViewStock.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridViewStock.Size = new System.Drawing.Size(948, 532);
             this.dataGridViewStock.TabIndex = 0;
             // 
