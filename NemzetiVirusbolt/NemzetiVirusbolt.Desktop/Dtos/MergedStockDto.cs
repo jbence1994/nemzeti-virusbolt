15be2f927005c88880eb7d2e9471b771dbc767d5
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-12-01 18:17:04 +0100

    Update MergedStockDto.cs

--- NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Dtos/MergedStockDto.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Dtos/MergedStockDto.cs
@@ -2,16 +2,7 @@
 {
     public class MergedStockDto
     {
-        public string ProductName { get; private set; }
-        public int Quantity { get; private set; }
-
-        public static MergedStockDto ToDto(string productName, int quantity)
-        {
-            return new MergedStockDto
-            {
-                ProductName = productName,
-                Quantity = quantity
-            };
-        }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
     }
 }
