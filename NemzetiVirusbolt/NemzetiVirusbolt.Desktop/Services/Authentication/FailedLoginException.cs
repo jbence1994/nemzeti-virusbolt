63e1bfa9eff7e9fd7f643bef82b296432a09b2e7
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-12-01 17:28:06 +0100

    Update FailedLoginException.cs

--- NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Services/Authentication/FailedLoginException.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.Desktop/Services/Authentication/FailedLoginException.cs
@@ -4,11 +4,5 @@ namespace NemzetiVirusbolt.Desktop.Services.Authentication
 {
     public sealed class FailedLoginException : Exception
     {
-        private const string ExceptionMessage = "Helytelen belépési adatok.";
-
-        public FailedLoginException()
-            : base(ExceptionMessage)
-        {
-        }
     }
 }
