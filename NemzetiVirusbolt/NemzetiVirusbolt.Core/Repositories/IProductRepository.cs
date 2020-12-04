e94e9755cdd38a576a4e7ec0e680cf17478d2838
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-12-04 23:35:49 +0100

    remove get product endpoint.

--- NemzetiVirusbolt/NemzetiVirusbolt.Core/Repositories/IProductRepository.cs
+++ NemzetiVirusbolt/NemzetiVirusbolt.Core/Repositories/IProductRepository.cs
@@ -7,7 +7,6 @@ namespace NemzetiVirusbolt.Core.Repositories
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProducts();
-        Task<Product> GetProduct(int id);
         Task AddProduct(Product product);
     }
 }
