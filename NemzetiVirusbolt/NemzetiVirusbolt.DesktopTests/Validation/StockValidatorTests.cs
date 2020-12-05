78304ced63340ab8c693c60e7f3ee0d08a014a4f
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-12-05 13:48:21 +0100

    add unit tests.

---
+++ NemzetiVirusbolt/NemzetiVirusbolt.DesktopTests/Validation/StockValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NemzetiVirusbolt.Desktop.Dtos;
+using NemzetiVirusbolt.Desktop.Validation;
+
+namespace NemzetiVirusbolt.DesktopTests.Validation
+{
+    [TestClass]
+    public class StockValidatorTests
+    {
+        [TestMethod]
+        public void StockValidatorTest_InCaseQuantityIsZero_ShouldHaveAtLeastOneError()
+        {
+            // Arrange
+
+            var stockValidator = new StockValidator();
+            var stockToSave = new SaveStockDto
+            {
+                ProductId = 1,
+                Quantity = 0
+            };
+
+            // Act
+
+            var validationResult = stockValidator.Validate(stockToSave);
+
+            // Assert
+
+            Assert.AreEqual(1, validationResult.Errors.Count);
+        }
+    }
+}
