ccf48114960a9821ef69a941f7523b3b7d705244
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-04-27 19:56:18 +0200

    Add shell script for generating code coverage report

---
+++ scripts/run-unit-test.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/bash
+
+cd ../NemzetiVirusbolt.Desktop.Tests
+
+rm -rf ./coverage
+
+dotnet test
+
+dotnet tool install -g dotnet-reportgenerator-globaltool
+
+dotnet test --collect:"XPlat Code Coverage"
+
+reportgenerator "-reports:./**/coverage.cobertura.xml" "-targetdir:./coverage" "-reporttypes:Html"
